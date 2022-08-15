c618646f6370a5a2d880e586cfabe8cacf2c9a2e
napking <43017012+napking@users.noreply.github.com>
2022-08-14 19:37:13 -0600

    make_mongo with ONLY api calls

--- src/data/make_mongodb.py
+++ src/data/make_mongodb.py
@@ -6,18 +6,14 @@ Created on Sun Aug 14 12:05:10 2022
 """
 
 import pymongo
-from pymongo import MongoClient, InsertOne
+from pymongo import MongoClient
+import requests
 import json
 from pathlib import Path
 from config.definitions import *
-
-
-def get_from_file():
-    json_path = DATA_DIR / 'raw/household-travel-survey-trips.json'
-    with open(json_path) as file:
-        return json.load(file)
     
-def update_mongo():
+def create_mongo():
+    # really only need to run this once to create the mongo server
     
     # TODO: don't hard code the password
     uri = "mongodb+srv://StOny:fdOLHZqoU8523gus@cluster0.jpevnvt.mongodb.net/?retryWrites=true&w=majority"
@@ -25,12 +21,22 @@ def update_mongo():
     client = MongoClient(uri)
     
     db = client.kingston_travel
-    collection = db.trips_fact
     
-    documents = get_from_file()
-    result = collection.insert_many(documents)
-    del documents
+    results = []
+    for collection_name in ['trips','households','persons']:
+        # generate unique url for each collection
+        url = "https://opendatakingston.cityofkingston.ca/api/v2/catalog/"\
+            "datasets/household-travel-survey-" + collection_name + \
+            "/exports/json?limit=-1&offset=0&timezone=UTC"
+        response = requests.get(url)
+        # convert the data to json
+        documents = response.json()
+        
+        # 
+        results.append(
+            db[collection_name + "_facts"].insert_many(documents)
+            )
     client.close()
-    return result
+    return results
     
     
